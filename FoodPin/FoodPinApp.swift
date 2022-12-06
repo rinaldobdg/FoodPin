d09e126650cee4b9862794d037c9850fc822e483
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-06 09:43:13 +0700

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Benny Rinaldo on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
