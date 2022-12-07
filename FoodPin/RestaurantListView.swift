e1e998635693b6bae898be77c93026a80f34005b
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-07 20:38:26 +0700

    FoodPin final version completed, but still revised later on.

---
+++ FoodPin/RestaurantListView.swift
@@ -0,0 +1,102 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Benny Rinaldo on 06/12/22.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney","New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Casual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var body: some View {
+        List {
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                BasicTextImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index])
+            }
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct RestaurantListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantListView()
+        
+        RestaurantListView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct BasicTextImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        HStack (alignment: .top, spacing: 20) {
+            Image(imageName)
+                .resizable()
+                //.scaledToFill()
+                .frame(width:120, height: 118)
+                .cornerRadius(20)
+            
+            VStack (alignment: .leading){
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+            }
+            //.padding(.horizontal)
+            //.padding(.bottom)
+        }
+    }
+}
+
+struct FullImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        VStack (alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .cornerRadius(20)
+            
+            VStack (alignment: .leading){
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+    }
+}
