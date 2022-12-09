3771abadb512b7f4efa8ab7382c6fb108f537cd9
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-09 20:49:49 +0700

    Adding favorite and alert message

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct RestaurantListView: View {
     
-    @State var restaurantIsFavorites = Array(repeating: false, count: 21)
-    
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
