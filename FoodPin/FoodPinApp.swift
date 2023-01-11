9fef1083a9d1dd9226563e393a18dd632c0d24f7
Benny Rinaldo <rinaldobdg@yahoo.com>
2023-01-11 19:24:48 +0700

    still revised the app

--- FoodPin/FoodPinApp.swift
+++ FoodPin/FoodPinApp.swift
@@ -9,6 +9,21 @@ import SwiftUI
 
 @main
 struct FoodPinApp: App {
+    
+    init() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(red: 218/255, green: 96/255, blue: 51/255, alpha: 1.0), .font: UIFont(name: "ArialRoundedMTBold", size: 35)!]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor(red: 218/255, green: 96/22, blue: 51/255, alpha: 1.0), .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
+        navBarAppearance.backgroundColor = .clear
+        navBarAppearance.backgroundEffect = .none
+        navBarAppearance.shadowColor = .clear
+        
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        UINavigationBar.appearance().compactAppearance = navBarAppearance
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             RestaurantListView()
